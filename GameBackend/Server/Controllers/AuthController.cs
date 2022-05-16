b7d6e4564497f8bd851bb68986337b54e3b62fc4
Arthuro Verissimo <arthurobdfv@gmail.com>
2022-05-16 13:25:33 +0100

    Created AuthController and JwtGenerator

---
+++ GameBackend/Server/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿
+using Google.Apis.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public class AuthenticateRequest
+        {
+            [Required]
+            public string IdToken { get; set; }
+        }
+
+        private readonly JwtGenerator _jwtGenerator;
+
+        public UserController(IConfiguration configuration)
+        {
+            _jwtGenerator = new JwtGenerator(configuration.GetValue<string>("JwtPrivateSigningKey"));
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] AuthenticateRequest data)
+        {
+            GoogleJsonWebSignature.ValidationSettings settings = new GoogleJsonWebSignature.ValidationSettings();
+
+            // Change this to your google client ID
+            settings.Audience = new List<string>() { "708313847097-qqhkk449k8ut39q0uf0290rhvgm4cthh.apps.googleusercontent.com" };
+
+            GoogleJsonWebSignature.Payload payload = GoogleJsonWebSignature.ValidateAsync(data.IdToken, settings).Result;
+            return Ok(new { AuthToken = _jwtGenerator.CreateUserAuthToken(payload.Email) });
+        }
+    }
+}
---
+++ GameBackend/Server/Utility/JwtGenerator.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
+
+namespace Server.Utility
+{
+    public class JwtGenerator
+    {
+        readonly RsaSecurityKey _key;
+        public JwtGenerator(string signingKey)
+        {
+            RSA privateRSA = RSA.Create();
+            privateRSA.ImportRSAPrivateKey(Convert.FromBase64String(signingKey), out _);
+            _key = new RsaSecurityKey(privateRSA);
+        }
+
+        public string CreateUserAuthToken(string userId)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Audience = "myApi",
+                Issuer = "AuthService",
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                            new Claim(ClaimTypes.Sid, userId.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(60),
+                SigningCredentials = new SigningCredentials(_key, SecurityAlgorithms.RsaSha256)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
